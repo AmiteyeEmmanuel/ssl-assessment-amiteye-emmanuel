05c7d98171d31798e24df30355067d9d5ffa88c0
Emmanuel Amiteye <emmanuelamiteye9@gmail.com>
2024-12-12 09:50:40 +0100

    initial commits

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,12 +3,12 @@ import InputForm from "./form";
 
 export default function Main() {
   return (
-    <section className="flex flex-col lg:flex-row w-full">
+    <section className="flex flex-col lg:flex-row w-full h-[880px]">
       <div className="order-2 lg:order-1 lg:w-1/2 w-full h-full">
          <InputForm />
       </div>
 
-      <div className="order-1 lg:order-2 lg:w-1/2 w-full h-full">
+      <div className="order-1 lg:order-2 lg:w-1/2 w-full h-[880px] pb-20 bg-[#1C4532]">
         <Content />
       </div>
     </section>
